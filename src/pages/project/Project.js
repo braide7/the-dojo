a06677f327d20bb9f31d621992ef119b0949ff0d
Braidenp <braidenp17@gmail.com>
2022-11-19 15:42:40 -0700

    added comments and project filters

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,8 +1,11 @@
 import { useParams } from "react-router-dom"
 import { useDocument } from '../../hooks/useDocument'
+import ProjectSummary from "./ProjectSummary"
+import ProjectComments from "./ProjectComments"
 
 // styles
 import './Project.css'
+
 
 
 export default function Project() {
@@ -18,7 +21,8 @@ export default function Project() {
 
   return (
       <div className="project-details">
-        <h1>{document.name}</h1>
+        <ProjectSummary project={document}/>
+        <ProjectComments project={document}/>
       </div>
   )
 }
